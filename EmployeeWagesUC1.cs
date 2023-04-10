d9e8a3656e6aecd8c4c6f5063680f6e7d894cef7
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-04-10 05:55:07 +0530

    [Ajay] Welcome to Employee Attendance

---
+++ EmployeeWagesUC1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageUCPrograms
+{
+    public class EmployeeWagesUC1
+    {
+        //Created method to check Employee is present or absent
+        public static void EmployeeUC1(int empCheck)
+        {
+            int IS_PRESENT = 1;
+            if (empCheck == IS_PRESENT)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -5,7 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Prgrams");
-            Console.ReadLine();
+            int option = Convert.ToInt32(Console.ReadLine());
+            //Switch case to call different method 
+            switch (option)
+            {
+                case 1:
+                    Random random = new Random();
+                    int empCheck = random.Next(0, 2);
+                    EmployeeWagesUC1.EmployeeUC1(empCheck);
+                    break;
+
+            }
         }
     }
 }
