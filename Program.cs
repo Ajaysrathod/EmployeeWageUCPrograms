f8223d4e6dc32f94b5dc489056d6d606a4a3e12e
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-04-10 06:11:34 +0530

    [Ajay] Welcome to Daily employee Wages

--- Program.cs
+++ Program.cs
@@ -1,10 +1,12 @@
 ﻿namespace EmployeeWageUCPrograms
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Employee Wages Prgrams");
+            Console.WriteLine("Please enter option given below");
+            Console.WriteLine("1:Employee is present or absent UC1\n2:Employee Wage UC2 ");
             int option = Convert.ToInt32(Console.ReadLine());
             //Switch case to call different method 
             switch (option)
@@ -14,6 +16,15 @@
                     int empCheck = random.Next(0, 2);
                     EmployeeWagesUC1.EmployeeUC1(empCheck);
                     break;
+                case 2:
+                    Random random1 = new Random();
+                    int empCheck1 = random1.Next(0, 2);
+                    EmployeeWagesUC2.EmployeeUC2(empCheck1);
+                    break;
+                default:
+                    Console.WriteLine("Please enter right option");
+                    break;
+
 
             }
         }
